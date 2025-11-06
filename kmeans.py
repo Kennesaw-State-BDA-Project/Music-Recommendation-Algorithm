6df8ff1d9f10c540973722b371005dfdb09c0793
Matthew Pfleger <mattpfleger20@gmail.com>
2025-11-06 17:45:27 -0500

    Making some changes
    Ran music_data_recommendation.py to get processed dataset. Updated kmeans.py to reflect the new processed dataset

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,7 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.datasets import make_blobs
 
-df = pd.read_csv('user_item_matrix.csv')
-print(df)
+df = pd.read_csv("processed_user_item_matrix.csv")
+print(df.head())
