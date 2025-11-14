ac2ba2550641534ced73df5c4b270d6223add43e
Matthew Pfleger <mattpfleger20@gmail.com>
2025-11-13 20:21:45 -0500

    Update kmeans.py
    Preprocessed the data further for k-means algo, also added data visualization

--- kmeans.py
+++ kmeans.py
@@ -1,7 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
+# Read in the processed data / Print head
 df = pd.read_csv("processed_user_item_matrix.csv")
-print(df.head())
+
+# Data preprocessing for kmeans
+user_ids = df['user_id']
+X = df.drop(columns= ['user_id'])
+
+# Standardization
+std_scaler = StandardScaler()
+X_scaled = std_scaler.fit_transform(X)
+
+# Dimensionality reduction
+pca = PCA(n_components=0.9)
+X_pca = pca.fit_transform(X_scaled)
+
+# Create a final DataFrame and save it
+pca_columns = [f"PC{i+1}" for i in range(X_pca.shape[1])]
+X_final = pd.DataFrame(X_pca, columns=pca_columns)
+X_final.insert(0, 'user_id', user_ids)
+
+
+print(X_final.head())
+X_features = X_final.drop(columns=['user_id'])
+
+# Visualize data
+fig = plt.figure(0)
+plt.grid(True)
+plt.scatter(X_features.iloc[:,0],X_features.iloc[:,1])
+plt.show()
